3eb5bb91541d543182fe634b1125f466aa7dd293
StarPandaBeg <kshmurakov@mail.ru>
2025-02-16 12:52:50 +0300

    feat: Заглушка под интеграцию с ИИ

--- frontend/src/app/(landing)/generate/components/prompt-form.tsx
+++ frontend/src/app/(landing)/generate/components/prompt-form.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { Hint } from "@/components/ui/hint";
+import { Input } from "@/components/ui/input";
 import {
   Select,
   SelectContent,
@@ -9,23 +16,52 @@ import {
   SelectItem,
   SelectTrigger,
 } from "@/components/ui/select";
-import { Paperclip, Send } from "lucide-react";
+import { Dialog } from "@radix-ui/react-dialog";
+import { Send } from "lucide-react";
 import { useSearchParams } from "next/navigation";
 import { useState } from "react";
 
 export default function PromptForm() {
   const params = useSearchParams();
   const [prompt, setPrompt] = useState(params.get("search") || "");
+  const [file, setFile] = useState<File | null>(null);
   const [slidesNumber, setSlidesNumber] = useState("10");
 
+  const [failDialog, setFailDialogOpen] = useState(false);
+
+  const handleSend = (e: any) => {
+    e.preventDefault();
+    console.log(file);
+    setFailDialogOpen(true);
+  };
+
   return (
-    <form className="w-full">
+    <form className="w-full" onSubmit={handleSend}>
+      <Dialog open={failDialog} onOpenChange={setFailDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Упс!</DialogTitle>
+          </DialogHeader>
+          <p>
+            Функционал ИИ-редактора разработан, но не был интегрирован на
+            платформу
+          </p>
+          <p>Простое создание презентации с чистого листа все еще работает!</p>
+          <DialogClose>
+            <Button variant={"secondary"}>Погрустить и закрыть</Button>
+          </DialogClose>
+        </DialogContent>
+      </Dialog>
       <div className="flex items-center justify-between gap-2 text-sm text-gray-300">
         <div className="space-x-2">
           <span>Напиши идею для своей презентации</span>
           <Hint text="Введите, что хотите видеть в презентации — от пары слов до детальных инструкций. Наш ИИ создаст для вас структуру слайдов."></Hint>
         </div>
-        <Select value={slidesNumber} onValueChange={(e) => setSlidesNumber(e)}>
+        <Select
+          value={slidesNumber}
+          onValueChange={(e) => setSlidesNumber(e)}
+          disabled
+        >
           <SelectTrigger className="w-min">
             <p className="px-2">
               <span className="font-bold">Количество слайдов:</span>{" "}
@@ -59,10 +95,19 @@ export default function PromptForm() {
             <span className="text-sm text-gray-500">
               Дополнительное содержимое (опционально):
             </span>
-            <Button variant="secondary">
-              <Paperclip />
-              Прикрепить
-            </Button>
+            <div className="flex flex-col items-end justify-center gap-2">
+              <Input
+                type="file"
+                className="w-min bg-secondary text-secondary-foreground"
+                onChange={(e) =>
+                  setFile(e.target.files ? e.target.files[0] : null)
+                }
+                accept="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+              />
+              <a href="#" className="text-sm text-gray-500 underline">
+                Пример файла
+              </a>
+            </div>
           </div>
         </div>
       </div>
